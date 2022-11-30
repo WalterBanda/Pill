f403989bff8d4258e5efb97066b0de039a5f003c
jerimkaura <jerimkaura001@gmail.com>
2022-11-30 20:41:31 +0300

    - blannk medication fragment

---
+++ app/src/main/java/dev/medtracker/client/presentation/medication/MedicationFragment.kt
@@ -0,0 +1,15 @@
+package dev.medtracker.client.presentation.medication
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import dev.medtracker.client.R
+import dev.medtracker.client.databinding.FragmentMedicationBinding
+
+class MedicationFragment : Fragment(R.layout.fragment_medication) {
+    private lateinit var binding: FragmentMedicationBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMedicationBinding.bind(view)
+    }
+}
