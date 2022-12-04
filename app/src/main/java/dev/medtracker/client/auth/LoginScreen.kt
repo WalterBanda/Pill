6f803d47a3a65df19eeff8d7560afbae588ed7c9
Walter Banda <walterkaimurima@gmail.com>
2022-12-04 20:07:00 +0300

    ☕ Added Login screen Fragment

---
+++ app/src/main/java/dev/medtracker/client/auth/LoginScreen.kt
@@ -0,0 +1,12 @@
+package dev.medtracker.client.auth
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import dev.medtracker.client.R
+
+class LoginScreen : Fragment(R.layout.fragment_auth_login_screen) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
